3324b124393b9af1413e35388d987757c4fcf73c
Cunxin Jia <imposimon@gmail.com>
2013-12-05 15:58:02 +0800

    add P06 solution

--- p01-10.scala
+++ p01-10.scala
@@ -32,11 +32,24 @@ object main extends App {
 		case head :: tail => reverse(tail) ::: List(head)
 	}
 
+	//P06 Find out whether a list is a palindrome
+	def isPalindrome[T](list: List[T]): Boolean = list match {
+		case Nil => true
+		case head :: tail => reverse(tail) match {
+				case Nil => true
+				case rhead :: rtail => isPalindrome(rtail) && (head == rhead)
+		}
+	}
+
 	//test cases	
 	val list1 = List(1, 2, 3)
 	val list2 = List('a', 'b', 'c')
 	val list3 = List(1)
 	val emptyList = List()
+	val palindromeList1 = List(1, 2, 3, 4, 5, 4, 3, 2, 1)
+	val palindromeList2 = List(1, 2, 3, 4, 4, 3, 2, 1)
+	val palindromeList3 = List(1, 1)
+	val palindromeList4 = List(1)
 
 	println(last(list1))
 	println(last(list2))
@@ -54,4 +67,10 @@ object main extends App {
 	println(length(emptyList))
 
 	println(reverse(list2))
+
+	println(isPalindrome(palindromeList1))
+	println(isPalindrome(palindromeList2))
+	println(isPalindrome(palindromeList3))
+	println(isPalindrome(palindromeList4))
+	println(isPalindrome(list1))
 }
